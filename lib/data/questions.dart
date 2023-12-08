219f26b91619fe3c6c44b7dc320d70a51c86dc63
FrostyNasty <aldo24569@gmail.com>
2023-12-08 10:20:50 +0700

    MVP

---
+++ lib/data/questions.dart
@@ -0,0 +1,55 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Apa rumah adat Provinsi Jawa Tengah?',
+    [
+      'Joglo',
+      'Gadang',
+      'Honai',
+      'Krong Bade',
+    ],
+  ),
+  QuizQuestion('Siapa nama presiden ketiga Indonesia?', [
+    'Habibie',
+    'Jokowi',
+    'Susilo Bambang Yudhoyono',
+    'Megawati',
+  ]),
+  QuizQuestion(
+    'Apa ibukota Jawa Tengah? ',
+    [
+      'Semarang',
+      'Jogjakarta',
+      'Solo',
+      'Surabaya',
+    ],
+  ),
+  QuizQuestion(
+    'Apa warna bendera Indonesia?',
+    [
+      'Merah & Putih',
+      'Hijau & Kuning',
+      'Merah & Biru',
+      'Putih & Abu - abu',
+    ],
+  ),
+  QuizQuestion(
+    'Kapan hari sumpah pemuda diperingati?',
+    [
+      '28 Oktober',
+      '28 September',
+      '28 Juni',
+      '29 Agustus',
+    ],
+  ),
+  QuizQuestion(
+    'Tinky Winky, Dipsy, Laa-Laa,....',
+    [
+      'Puh... Sepuh...',
+      'Asep',
+      'Po',
+      'Lii-Lii',
+    ],
+  ),
+];
